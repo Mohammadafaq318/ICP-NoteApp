1e90b50e1ede64c5c400804b594980f79d31fcf7
moaf <mafaqq318@gmail.com>
2023-09-07 14:33:49 +0300

    ReactJs Template for Starting

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import reactDom from "react-dom";
+
+reactDom.render(
+  <div>
+    <h1 style={{ color: "red" }}>My Favourite Drinks</h1>
+  </div>,
+  document.getElementById("root")
+);
