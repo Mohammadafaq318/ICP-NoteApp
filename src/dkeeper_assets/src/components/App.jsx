1e1bbf573f9c3fb3eb62f511dd50966290f80730
moaf <mafaqq318@gmail.com>
2023-09-18 13:02:55 +0300

    Added Keeper to ICP Blockchain

---
+++ src/dkeeper_assets/src/components/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+
+  const [Notes, setNotes] = React.useState([]);
+
+
+  function addNote(note){
+    setNotes((prevItems) => [...prevItems, note]);
+  }
+
+  function deleteNote(index){
+    setNotes((prevItems) => {
+      const updatedNotes = [...prevItems];
+      updatedNotes.splice(index, 1);
+  
+      return updatedNotes;
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {Notes.map((item, index) => (
+        <Note deleteItem={deleteNote} key={index} id={index} title={item.title} content={item.content} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dkeeper_assets/src/components/Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Note(props) {
+
+  function removeitem(event){
+    event.preventDefault();
+    props.deleteItem(props.id);
+  }
+
+
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={removeitem}>DELETE</button>
+    </div>
+  );
+}
+
+export default Note;
